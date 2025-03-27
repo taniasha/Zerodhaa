f96aa64616fd8a6a00df67a953684e7a75391801
tania sharma <bishupandit07@gmail.com>
2025-03-27 18:07:42 +0530

    update

--- frontend/src/Landing_page/Navbar.js
+++ frontend/src/Landing_page/Navbar.js
@@ -23,11 +23,11 @@ export default function Navbar() {
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
+                {/* <li class="nav-item">
                   <Link class="nav-link " aria-current="page" to={"/signup"}>
                     Signup
                   </Link>
-                </li>
+                </li> */}
                 <li class="nav-item">
                   <Link class="nav-link" to={"/about"}>
                     About
--- frontend/src/Landing_page/signup/Signup.js
+++ frontend/src/Landing_page/signup/Signup.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Signup() {
   return (
     <div>
-      <h1>helo</h1>
+      {/* <h1>helo</h1> */}
     </div>
   )
 }
